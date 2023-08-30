dca8ddadcd87b18176f222859f634979245f1c99
Santiago Muñoz <josmansant@unicauca.edu.co>
2023-08-30 18:05:18 -0500

    se agregan eventos tales carga de imagen carga de los chat y carga de la imagen del perfil en un chat selecionado presenta errores en pintar el chat selecionado aun

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
 import { getMessages } from './getMessages.js';
 import { getUsers } from './getUsers.js';
-
+import { updateMessage } from './updateMessage.js';
 const params = new URLSearchParams(window.location.search);
 const ID = params.get('id');
+let ID2 = 0;
 const SEND_MESSAGE = document.querySelector('.container__whatsapp__myChat__barraChats__formulario-input');
 const IMG_SEND = document.querySelector('.container__whatsapp__myChat__barraChats__formulario-send');
-const CHANGE_PERSONAL_INFORMATION = document.querySelector('.container__whatsapp__myContacts__header-user');
+
 const TEMPLATE_CHANGE_IMAGE = document.querySelector('.container__whatsapp__changeImagen');
 const TEMPLATE_MY_CONTACTS = document.querySelector('.container__whatsapp__myContacts');
 const ARROW_BACK_PERFIL = document.querySelector('.container__whatsapp__changeImagen__header__imgPerfil-arrow');
@@ -20,6 +21,11 @@ const CONTAINER_RECEIVE_MESSAGE = document.querySelector('.container__whatsapp__
 const ARROW_DOWN_RECEIVE = document.querySelector('.container__whatsapp__myChat__fondo__mensajeRecibido__contenedor__message-arrow');
 const LIST_MESSAGE_SEND = document.querySelector('.container__whatsapp__myChat__fondo__mensajeEnviado__contenedor__message-arrow-lista');
 const LIST_MESSAGE_RECEIVE = document.querySelector('.container__whatsapp__myChat__fondo__mensajeRecibido__contenedor__message-arrow-lista');
+const CONTAINER_DARD = document.querySelector('.container__card')
+const CONTAINER_HEADER_USER = document.querySelector('.container__whatsapp__myContacts__header');
+
+let listMessages = await getMessages();
+let listUsers = await getUsers();
 
 /*Cambiar icono, cuanto este escribiendo un mensaje */
 SEND_MESSAGE.addEventListener('input', () => {
@@ -31,10 +37,25 @@ SEND_MESSAGE.addEventListener('input', () => {
 
 });
 
-/*Dar click en la foto, para cambiar los datos */
-CHANGE_PERSONAL_INFORMATION.addEventListener('click', () => {
-    TEMPLATE_CHANGE_IMAGE.style.display = 'block';
-    TEMPLATE_MY_CONTACTS.style.display = 'none';
+/*Enviar Mensaje del usuario con sesion iniciada al del chat abierto*/
+SEND_MESSAGE.addEventListener('keypress',async (e) => {
+    if (e.key === 'Enter') {
+        let user = listUsers.find(user => user.id == ID);
+        let user2 = listUsers.find(user => user.id == ID2);
+        let message= listMessages.find(message => message.idUser1 == ID && message.idUser2 == ID2 || message.idUser1 == ID2 && message.idUser2 == ID);
+        message.conversaciones.push(
+            {
+                "sendBy": ID,
+                "date": "2017-10-10",
+                "hour": "10:11:10",
+                "message": SEND_MESSAGE.value,
+                "flag": false
+            }
+        );
+       let data=updateMessage(parseInt(ID),parseInt(ID2), message.id,message.conversaciones);
+        SEND_MESSAGE.value = "";
+        listMessages = await getMessages();
+    }
 });
 
 /*Dar click en arrow, para volver al home */
@@ -45,12 +66,11 @@ ARROW_BACK_PERFIL.addEventListener('click', () => {
 
 /*Editar el nombre */
 EDIT_NAME.addEventListener('click', () => {
-    INPUT_EDIT.readOnly = false;
+    INPUT_EDIT.readOnly = true;
     INPUT_EDIT.style.border = "2px solid black";
 });
 
 /*Templete de buscar los mensajes */
-
 SEND_MESSAGE_MY_CHAT.addEventListener('click', () => {
     TEMPLETE_INFO_MESSAGE.style.display = 'block';
 });
@@ -58,6 +78,7 @@ SEND_MESSAGE_MY_CHAT.addEventListener('click', () => {
 CLOSE_TEMPLETE_INFO_MESSAGE.addEventListener('click', () => {
     TEMPLETE_INFO_MESSAGE.style.display = 'none';
 });
+
 /*Mostrar y desaparecer la flecha de opciones de los mensajes enviados */
 CONTAINER_SEND_MESSAGE.addEventListener('mouseover', () => {
     ARROW_DOWN_SEND.style.display = 'block';
@@ -87,23 +108,27 @@ ARROW_DOWN_RECEIVE.addEventListener('dblclick', () => {
     LIST_MESSAGE_RECEIVE.style.display = 'none';
 });
 
+/*Cargar la foto de perfil del usuario que inicio sesion */
+const LOADING_IMAGE_PROFILE = async () => {
+    let user = listUsers.find(user => user.id == ID);
+    const img = `<img class="container__whatsapp__myContacts__header-user" src="${user.urlImgPerfil}" alt="userMyContacts"></img>`
+    CONTAINER_HEADER_USER.innerHTML = img;
 
+}
+/*Listar la lista de mensajes de cada usuario */
 const LIST_MY_CHAT = async () => {
-    let listMessages = await getMessages();
-    let listUsers = await getUsers();
+
     let user = listUsers.find(user => user.id == ID);
     let mylistMessages = listMessages.filter(index => index.idUser1 == ID || index.idUser2 == ID)
     let html = ``;
     let user2;
     mylistMessages.forEach((message) => {
-        
-        if(parseInt(ID) === message.idUser1){
+        if (parseInt(ID) === message.idUser1) {
             user2 = listUsers.find(index => index.id == message.idUser2);
-       }else{
+        } else {
             user2 = listUsers.find(index => index.id == message.idUser1);
-            
-       }
-        const DIV = `<div class="container__whatsapp__myContacts__cardContact">
+        }
+        const DIV_CARD = `<div id ="${user2.id}" class="container__whatsapp__myContacts__cardContact">
         <img class="fas fa-search container__whatsapp__myContacts__cardContact-imagen" src=${user2.urlImgPerfil} alt="userContact">
         <div class="container__whatsapp__myContacts__cardContact__text">
             <div class="container__whatsapp__myContacts__cardContact__text__nameDate">
@@ -118,19 +143,52 @@ const LIST_MY_CHAT = async () => {
             </div>
         </div>
     </div>`;
-        html += DIV;
+        html += DIV_CARD;
     })
-    const div = document.querySelector('.container__card')
-    div.innerHTML = html;
+    CONTAINER_DARD.innerHTML = html;
 }
 
-const MY_CONTACTS_CARD = document.querySelector('.container__whatsapp__myContacts__cardContact');
+const LOADDING_CHAT = async (idUser2) => {
+    let user = listUsers.find(user => user.id == ID);
+    let user2=listUsers.find(user => user.id == idUser2);
+    const div = document.querySelector('.container__whatsapp__myChat__header__imgUserLine');
+    const IMAGE_PERFIL =                        
+    `<img class="container__whatsapp__myChat__header__imgUserLine-img" src="${user2.urlImgPerfil}" alt="user_chat">
+    <div class="container__whatsapp__myChat__header__imgUserLine__userLine">
+        <h5 class="container__whatsapp__myChat__header__imgUserLine__userLine-name">${user2.nombre}</h5>
+        <h6 class="container__whatsapp__myChat__header__imgUserLine__userLine-line"> EN LÍNEA</h6>
+    </div>`
+    div.innerHTML = IMAGE_PERFIL;
+}
+const LOADING_MESSAGES = async () => {
+    
+}
+
+LOADING_IMAGE_PROFILE();
+LIST_MY_CHAT();
+
+
+/*Los siguientes container los cargamos aqui y no en el principio de la script para no generar errores*/
+const MY_CONTACTS_CARD = document.querySelectorAll('.container__whatsapp__myContacts__cardContact');
+const CHANGE_PERSONAL_INFORMATION = document.querySelector('.container__whatsapp__myContacts__header-user');
+
+
+console.log(MY_CONTACTS_CARD)
 /*Poner en otro color cuando se seleccione una card */
-MY_CONTACTS_CARD.addEventListener('click', () => {
-    console.log('cards:  ', MY_CONTACTS_CARD)
-    MY_CONTACTS_CARD.style.backgroundColor = " rgb(237, 237, 237)";
+CONTAINER_DARD.addEventListener('click', () => {
+
+    MY_CONTACTS_CARD.forEach((card) => {
+        card.addEventListener('click', () => {
+            card.style.backgroundColor = '#e9e9e9';
+            ID2= parseInt(card.id);
+            LOADDING_CHAT(ID2);
+        });
+    });
 });
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    LIST_MY_CHAT();
+/*Dar click en la foto, para cambiar los datos */
+CHANGE_PERSONAL_INFORMATION.addEventListener('click', () => {
+    TEMPLATE_CHANGE_IMAGE.style.display = 'block';
+    TEMPLATE_MY_CONTACTS.style.display = 'none';
+
 });
--- script/updateMessage.js
+++ script/updateMessage.js
@@ -1,8 +1,8 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
 
-export async function updateMessage(mensaje){
+export async function updateMessage(id1,id2 ,id,mensaje){
     try {
-        const response = await axios.put(`http://localhost:3000/mensajes/${mensaje.id},{ mensaje }`);
+        const response = await axios.put(`http://localhost:3000/mensajes/${id}`,{idUser1:id1,idUser2:id2, conversaciones:mensaje });
         console.log("response update", response.data)
     }catch (error) {
         console.error('Error updating mensaje:', error);
